592a2198d8aa31238f35dd8895a58e8f53d203d1
saher saleem <saleemsaba281@outlook.com>
2024-03-04 22:55:16 +0500

    fourth commit

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ import inquirer from "inquirer"
 import chalk from "chalk"
 import chalkAnimation from "chalk-animation"
 
-const sleep = ()=>{
-    return new Promise((res)=>{
-    setTimeout(res,2000)
-    
-     } )
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000)
+
+    })
 }
 
-async function start () {
+async function start() {
     let gameTITLE = chalkAnimation.rainbow("Let's play a game !")
     await sleep()
     gameTITLE.stop
@@ -19,32 +19,40 @@ async function start () {
 
 
 async function guessNumber() {
-  
+
     await start()
-    const systemGen = (Math.floor (Math.random()*10))
-    const personGuess = 
-  await  inquirer .prompt([
-         {type:"number",
-        name :"GuessNum",
-         message:"Write a number between 1 to 10:"
+    const systemGen = (Math.floor(Math.random() * 10))
+    const personGuess =
+        await inquirer.prompt([
 
 
-}
 
-   ])
 
-   const printSystem= systemGen;
-   const user = personGuess.GuessNum
-   console.log(`System Generated number = ${printSystem}`)
-   console.log(`Your guess = ${user}`)
-if (systemGen==personGuess.GuessNum){
- console.log(chalk.bgBlueBright("YOHOO CONGRATULATIONS ! YOU WON"))
- console.log("You got 10 points")
-}
 
-else (
-    console.log(chalk.bgMagentaBright("TRY! NEXT TIME "))
-)
+
+
+            {
+                type: "number",
+                name: "GuessNum",
+                message: "Write a number between 1 to 10:"
+
+
+            }
+
+        ])
+
+    const printSystem = systemGen;
+    const user = personGuess.GuessNum
+    console.log(`System Generated number = ${printSystem}`)
+    console.log(`Your guess = ${user}`)
+    if (systemGen == personGuess.GuessNum) {
+        console.log(chalk.bgBlueBright("YOHOO CONGRATULATIONS ! YOU WON"))
+        console.log("You got 10 points")
+    }
+
+    else (
+        console.log(chalk.bgMagentaBright("TRY! NEXT TIME "))
+    )
 
 }
 
@@ -52,19 +60,19 @@ else (
 //guessNumber()
 
 async function restart() {
-    
 
-do {
-   await guessNumber()
-   var again=  await inquirer
-   .prompt([
-    {
-        type:"input",
-        name : "restart",
-        message:chalk.red("Do You want to continue Press y :")
-    }
-   ])
- } while(again.restart=="yes"||again.restart=="Yes"||again.restart=="y"||again.restart=="Yes")
+
+    do {
+        await guessNumber()
+        var again = await inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "restart",
+                    message: chalk.red("Do You want to continue Press y :")
+                }
+            ])
+    } while (again.restart == "yes" || again.restart == "Yes" || again.restart == "y" || again.restart == "Yes")
 
 }
 
